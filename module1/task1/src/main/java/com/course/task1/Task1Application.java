d657c9093891b0967894431962728c5dfd3f6ef2
Olga_Bamburova <Olga_Bamburova@epam.com>
2025-05-08 21:55:22 +0400

    task1: hello world application

--- module1/task1/src/main/java/com/course/task1/Task1Application.java
+++ module1/task1/src/main/java/com/course/task1/Task1Application.java
@@ -1,13 +1,18 @@
 package com.course.task1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Task1Application {
+public class Task1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Task1Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		System.out.println("hello world");
+	}
 }
